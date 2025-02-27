0f3d9746bb6e21fbfaeb5f61f97b49c840f0c9e4
Eduardo Luiz Franceliano <159642647+Eduardoluizfrancelianoda@users.noreply.github.com>
2025-02-27 08:56:59 -0300

    Add files via upload

---
+++ aula03/processa-exec01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome= $_POST['cxnome'];
+    $mensagem= $_POST['cxmensagem'];
+
+    echo "nome preenchido: $nome <br>";
+    echo "idade preenchida: $mensagem <br>";
+
+?>
---
+++ aula03/processa01.php
@@ -0,0 +1,8 @@
+<?php
+    $nome= $_REQUEST['cxnome'];
+    $idade= $_REQUEST['cxidade'];
+
+    echo "nome preenchido: $nome <br>";
+    echo "idade preenchida: $idade <br>";
+
+?>
